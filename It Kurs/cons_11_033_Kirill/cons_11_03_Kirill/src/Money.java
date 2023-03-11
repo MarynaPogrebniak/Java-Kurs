fc6baaa4af3e4c3c5824f7184486662896bbe06d
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-11 16:20:58 +0100

    choose class members

--- It Kurs/cons_11_033_Kirill/cons_11_03_Kirill/src/Money.java
+++ It Kurs/cons_11_033_Kirill/cons_11_03_Kirill/src/Money.java
@@ -1,7 +1,30 @@
 public class Money {
 
-  private long amount; // в копейках - мин целых единицах
-  private String currency; //валюта
+  private long amount; // в "копейках" - минимальных целых единицах
+  final private String currency; // валюта
 
+  public Money(double amount, String currency) {
+    setAmount(amount);
+    this.currency = currency;
+  }
 
+  public void setAmount(double amount) {
+    this.amount = Math.round(amount * 100); // переводим "рубли" в "копейки"
+  }
+
+  // нет setCurrency() - нельзя поменять валюту, она final
+
+  // "фальшивый" геттер - возвращаем "рубли" в виде double,
+  // а хранить продолжаем "копейки" в виде long
+  public double getAmount() {
+    return amount / 100.0;
+  }
+
+  // нет getCurrency() - отдельно валюта нам не нужна
+
+  // "фальшивый" геттер
+  // вернём красивую строку для печати
+  public String getMoneyString() {
+    return String.format("%.2f %s", getAmount(), currency);
+  }
 }
