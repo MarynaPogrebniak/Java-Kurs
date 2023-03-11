2c2e09e940d3081b4ab212299fee5bf57751b01c
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-11 10:27:19 +0100

    choose class members

---
+++ It Kurs/cons_11_033_Kirill/cons_11_03_Kirill/src/Item.java
@@ -0,0 +1,12 @@
+public class Item {
+  // артикул - уникальная строка
+  final private String article;
+  // название - строка
+  private String title;
+  // стоимость - специальный класс Деньги
+  private Money price;
+
+
+
+
+}
---
+++ It Kurs/cons_11_033_Kirill/cons_11_03_Kirill/src/Money.java
@@ -0,0 +1,7 @@
+public class Money {
+
+  private long amount; // в копейках - мин целых единицах
+  private String currency; //валюта
+
+
+}
