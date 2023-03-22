67913bd42e50b4552935e954ac493ee2e5bb0807
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-22 08:56:26 +0100

    add Lists und method swim

--- It Kurs/lesson_44_20_03_Interfaces/code/interfaces/src/homework/Runner.java
+++ It Kurs/lesson_44_20_03_Interfaces/code/interfaces/src/homework/Runner.java
@@ -1,4 +1,8 @@
 package homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //Задача 1.
 //Напишите интерфейсы "Умеет летать" CanFly с методом fly(), "Умеет бегать" CanRun с методом run
 // и "умеет плавать" CanSwim с методом swim и реализуйте их в классе "Супермен" Superman.
@@ -10,16 +14,57 @@ public class Runner {
 
   public static void main(String[] args) {
 
-    Airplane testAirplane = new Airplane();
-    Duck testDuck = new Duck();
-    Human testHuman = new Human();
-    Pinguin testPinguin = new Pinguin();
-    Superman testSuperman = new Superman();
+    List<CanFly> flyable = new ArrayList<>();
+    List<CanRun> runnable = new ArrayList<>();
+    List<CanSwim> swimmable = new ArrayList<>();
 
-    testAirplane.canFly();
-    testDuck.canSwim();
-    testHuman.canRun();
-    testPinguin.canSwim();
-    testSuperman.canFly();
+
+    Airplane testAirplane = new Airplane();
+    flyable.add(testAirplane);
+
+    Duck testDuck = new Duck();
+    flyable.add(testDuck);
+    runnable.add(testDuck);
+    swimmable.add(testDuck);
+
+    Human testHuman = new Human();
+    runnable.add(testHuman);
+    swimmable.add(testHuman);
+
+    Pinguin testPinguin = new Pinguin();
+    swimmable.add(testPinguin);
+    runnable.add(testPinguin);
+
+    Superman testSuperman = new Superman();
+    flyable.add(testSuperman);
+    swimmable.add(testSuperman);
+    runnable.add(testSuperman);
+
+    System.out.println("Все полетели и я полетел!");
+    for (CanFly flyingObject : flyable) {
+      flyingObject.canFly();
+      // flyingObject.run(); // ошибка, мы ничего не знаем про run, у нас List<CanFly>
+      if (flyingObject instanceof Superman superman) {
+        System.out.print("ЭТО БЫЛ СУПЕРМЕН! Он умеет бегать: ");
+        superman.canRun();
+      }
+    }
+    System.out.println();
+
+    System.out.println("Все побежали и я побежал!");
+    for (CanRun runningObject : runnable) {
+      runningObject.canRun();
+    }
+    System.out.println();
+
+    swim(swimmable);
+  }
+
+  public static void swim(List<CanSwim> swimmable) {
+    System.out.println("Все поплыли и я поплыл...");
+    for (CanSwim swimmingObject : swimmable) {
+      swimmingObject.canSwim();
+    }
   }
 }
+
