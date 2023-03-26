0ff946ffe400aaca0b80d95ac70375d9b4cf5ec5
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-26 14:41:39 +0200

    add BookAuthorPagesComparator

---
+++ It Kurs/lesson_46_22_03_Comparator/book_comparator/src/BookAuthorPagesComparator.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class BookAuthorPagesComparator implements Comparator<Book> {
+
+  @Override
+  public int compare(Book o1, Book o2) {
+    if (!o1.getAuthor().equals(o2.getAuthor())) {
+      return o1.getAuthor().compareTo(o2.getAuthor());
+    }
+//    return o1.getPages() - o2.getPages(); // сортировка по возрастанию страниц
+    return -(o1.getPages() - o2.getPages()); // сортировка по убыванию страниц
+  }
+}
