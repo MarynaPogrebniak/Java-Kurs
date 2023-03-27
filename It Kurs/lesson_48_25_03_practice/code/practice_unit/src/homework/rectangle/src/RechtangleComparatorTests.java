b9b12b723101f4ac3af9e1065f108104199b1491
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-27 16:48:50 +0200

    add one Test

--- It Kurs/lesson_48_25_03_practice/code/practice_unit/src/homework/rectangle/src/RechtangleComparatorTests.java
+++ It Kurs/lesson_48_25_03_practice/code/practice_unit/src/homework/rectangle/src/RechtangleComparatorTests.java
@@ -1,7 +1,15 @@
 import org.testng.annotations.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class RechtangleComparatorTests {
 
+  @Test
+  public void addPositiveIntegers(){
+  Rechtangle first = new Rechtangle(2,3);
+  int result = first.square();
+  assertEquals(6,result);
+}
 
 
 }
