68567ddf24f0d6f04a517fc54499d83925561e66
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-03-27 16:50:34 +0200

    add one Test

--- It Kurs/lesson_48_25_03_practice/code/practice_unit/src/homework/rectangle/src/RechtangleComparatorTests.java
+++ It Kurs/lesson_48_25_03_practice/code/practice_unit/src/homework/rectangle/src/RechtangleComparatorTests.java
@@ -7,8 +7,12 @@ public class RechtangleComparatorTests {
   @Test
   public void addPositiveIntegers(){
   Rechtangle first = new Rechtangle(2,3);
-  int result = first.square();
-  assertEquals(6,result);
+  Rechtangle second = new Rechtangle(3,2);
+
+    int resultFirst = first.square();
+    int resultSecond = second.square();
+    assertEquals(6,resultFirst);
+    assertEquals(6, resultSecond);
 }
 
 
