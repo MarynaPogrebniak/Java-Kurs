9f02ed47723bf6bd928889d057daaaedce5a0546
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-05 10:32:30 +0200

    linear_search

---
+++ It Kurs/lesson_50_28_03/code/linear_search/src/Task5RLE.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task5RLE {
+
+    // Run-length encoding (RLE) (aaabbcddddaaaaa = a3b2cd4a5)
+    public static void main(String[] args) throws IOException { // O(n) - time, O(n) - space
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите строку для сжатия:");
+        String input = br.readLine(); // O(n) - time, O(n) - space
+        StringBuilder result = new StringBuilder(); // класс для "изменяемых строк", почти коллекция
+
+        Character prev = null; // предыдущий символ, в начале он null - его нет
+        int counter = 0; // счётчик текущего символа
+        for (int i = 0; i < input.length(); ++i) {
+            Character c = input.charAt(i); // текущий символ // br.read() - не работает с Unicode
+            if (!c.equals(prev)) { // если текущий не совпадает с предыдущим
+                // значит, закончилось повторение предыдущего символа
+                // сам предыдущий символ уже записан в строке при его появлении
+                // нам, возможно, надо записать число его повторений
+                if (counter > 1) { // 0 и 1 записывать не нужно
+//          result.append(counter);
+                    System.out.print(counter); // эффективнее по памяти
+                }
+
+//        result.append(c); // записываем текущий (не совпадающий) в результат
+                System.out.print(c);
+                prev = c;
+                counter = 1; // 1 символ мы уже прочитали - символ стал "предыдущим"
+                // String += String / char -- O(n) для ОДНОГО сложения
+                // StringBuilder.append - O(1)
+            } else {
+                ++counter;
+            }
+        }
+        // в цикле мы не записали количество повторений последнего символа - он ни разу не становился
+        // "старым, не совпадающим"
+        // При этом в counter записано правильное значение
+        if (counter > 1) { // 0 и 1 записывать не нужно
+//      result.append(counter);
+            System.out.print(counter);
+        }
+        System.out.println();
+
+//    System.out.println("Промежуточный результат сжатия:");
+        System.out.println(result/*.toString()*/); // O(n) - space
+    }
+}
---
+++ It Kurs/lesson_50_28_03/code/linear_search/src/Utils.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+
+    // При расчёте сложности:
+    // выражения в скобках у O замечательно складываются и умножаются
+    // из полученного выражения выбрасываются "младшие" слагаемые - остаётся только самая
+    // "большая" функция (большая - значит, на графике она выше)
+    // после этого выбрасываются все числовые множители (50 * 3 * n^2 --> n^2)
+    // выражение T(n) относится к группе O(f(n)), если существует число c, такое, что
+    // T(n) < c * f(n) для больших n
+
+    // O(5 + n + 1) = O(n + 6) = O(n)
+    public static List<Integer> readList() throws IOException {
+        List<Integer> result = new ArrayList<>(); // O(1)
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // O(1)
+
+        System.out.print("Введите количество чисел: "); // O(1)
+        int n = Integer.parseInt(br.readLine()); // O(1)
+        System.out.println("Введите " + n + " чисел, каждое с новой строки:"); // O(1)
+        for (int i = 0; i < n; ++i) { // O((1 + 1) * n) = O(2 * n) = O(n)
+            int x = Integer.parseInt(br.readLine()); // O(1)
+            result.add(x); // O(1)
+        }
+
+        return result; // O(1)
+    }
+}
