920192e223db623b67c5a9b446aa1df213bc3556
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-06 08:59:54 +0200

    add lesson_56

---
+++ It Kurs/lesson_56_05_04_binary_search/code/binary_search/src/Task1GuessNumber.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Task1GuessNumber {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Загадайте число от 1 до 1000.");
+        System.out.println("На вопросы отвечайте символами '>', '<' или '=':");
+        System.out.println("'>', если ваше число больше предложенного;");
+        System.out.println("'<', если ваше число меньше предложенного;");
+        System.out.println("'=', если мы угадали ваше число.");
+
+        int result = guessNumber(br);
+        System.out.println("Вы загадали число " + result + "!");
+    }
+
+    // На каждом шаге область поиска уменьшается в два раза
+    // в итоге размер должен стать 1
+    // значит, 1 * 2 * 2 * 2 ... = изначальный размер области поиска,
+    // где количество двоек в произведении - это количество шагов
+
+    // Если количество шагов обозначить K, то получится
+    // 1 * (2 ^ k) = N
+    // 2 ^ k = N
+    // Логарифм - степень, в которую нужно возвести основание, чтобы получить аргумент.
+    // k - степень, в которую нужно возвести 2, чтобы получить N.
+    // k = log2(N) (логарифм по основанию 2)
+    // O(log N) - сложность по времени - быстрее, чем O(n)
+    // O(1) - сложность по памяти
+    private static int guessNumber(BufferedReader br) throws IOException {
+        int step = 1;
+        int left = 1;
+        int right = 1000;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            System.out.println(step + ". Ваше число - " + mid + "?");
+            String answer = br.readLine();
+            switch (answer) {
+                // искомое число больше mid, то есть в диапазоне от (mid + 1) до right
+                case ">" -> left = mid + 1;
+                // искомое число меньше mid, то есть в диапазоне от left до (mid - 1)
+                case "<" -> right = mid - 1;
+                case "=" -> {
+                    System.out.println("Ура!");
+                    return mid;
+                }
+                default -> {
+                    System.out.println("Вы ввели некорректный ответ, введите '>', '<' или '='");
+                    --step; // поправка на "пустой" шаг
+                }
+            }
+            ++step;
+        }
+        // сюда мы попадём, если left совпал с right - это и есть ответ
+
+        return left;
+    }
+}
---
+++ It Kurs/lesson_56_05_04_binary_search/code/binary_search/src/Task2IndexOf.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Task2IndexOf {
+
+    /**
+     * Поиск числа target в списке numbers
+     * @param numbers отсортированный по возрастанию список различных целых чисел
+     * @param target число, которое необходимо найти
+     * @return индекс числа target в списке numbers или -1, если число не найдено
+     */
+    public static int indexOf(ArrayList<Integer> numbers, int target) { // O(1) по памяти
+        // считаем сложность по времени
+        // индексы left и right сделаем "как везде" - left включая, right - не включая
+        int left = 0;
+        int right = numbers.size(); // опасность! но для библиотечных коллекций это O(1)
+        // пока размер области поиска больше одного элемента
+        while (left < right - 1) { // right - left > 1 // O(log N)
+            int mid = (left + right) / 2;
+            // ArrayList.get() - сложность O(1)
+            // LinkedList.get() - сложность O(n)
+            if (numbers.get(mid) == target) {
+                return mid;
+            }
+            // ArrayList.get() - сложность O(1)
+            // LinkedList.get() - сложность O(n)
+            if (numbers.get(mid) < target) {
+                left = mid + 1;
+            } else { // не = и не <
+                right = mid; // потому что right - не включая
+            }
+        }
+        // после окончания цикла right - left = 0 или 1, и проверяем мы numbers[left]
+        if (left < numbers.size() && numbers.get(left) == target) {
+            // ArrayList.get() - сложность O(1)
+            // LinkedList.get() - сложность O(n)
+            return left;
+        }
+        return -1; // сужение области поиска не дало нам target - его нет в списке
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите количество элементов: ");
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> numbers = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i) { // O(n) - time, O(n) - space
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+
+        System.out.print("Введите число для поиска: ");
+        int target = Integer.parseInt(br.readLine());
+
+        System.out.println("Результат: " + indexOf(numbers, target));
+    }
+}
