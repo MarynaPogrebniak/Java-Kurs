b61a0d2335038e3371f9943fe3ffcd05eaa53786
Maryna Porebniak <ms.pogrebnyak@gmail.com>
2023-04-06 10:09:36 +0200

    add Homework1

---
+++ It Kurs/lesson_56_05_04_binary_search/code/binary_search/src/homework_56/Homework1.java
@@ -0,0 +1,56 @@
+package homework_56;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Homework1 {
+    //Задача 1.
+    //Напишите метод для поиска числа в заранее отсортированном по убыванию
+    // списке различных целых чисел.
+    //
+    //Метод должен вернуть индекс, если элемент найден, и -1, если элемент не найден.
+    //
+    //Используйте в качестве образца задачу 2 из классной работы.
+
+    public static int indexOf(ArrayList<Integer> numbers, int target) { // O(1) по памяти
+
+        int left = 0;
+        int right = numbers.size();
+        while (left < right - 1) {
+            int mid = (left + right) / 2;
+
+            if (numbers.get(mid) == target) {
+                return mid;
+            }
+            if (numbers.get(mid) < target) {
+                right = mid;
+            } else { // не = и не < numbers.get(mid) > target
+
+                left = mid+1;// потому что right - не включая
+            }
+        }
+        // после окончания цикла right - left = 0 или 1, и проверяем мы numbers[left]
+        if (left < numbers.size() && numbers.get(left) == target) {
+            return left;
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Введите количество элементов: ");
+        int n = Integer.parseInt(br.readLine());
+
+        ArrayList<Integer> numbers = new ArrayList<>(n);
+        System.out.println("Введите числа: ");
+        for (int i = 0; i < n; ++i) { // O(n) - time, O(n) - space
+            numbers.add(Integer.parseInt(br.readLine()));
+        }
+
+        System.out.print("Введите число для поиска: ");
+        int target = Integer.parseInt(br.readLine());
+
+        System.out.println("Результат: " + indexOf(numbers, target));
+    }
+}
